952b7806cfe169bbf18db49cdebf2d259f184fc7
Axel Van Gestel <r0784084@student.thomasmore.be>
2021-10-08 11:18:11 +0200

    Initial Java project

---
+++ src/main/java/fact/it/back_playerdata_microservice/BackPlayerDataMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.back_playerdata_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPlayerDataMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackPlayerDataMicroserviceApplication.class, args);
+    }
+
+}
