152838b2a83a42f80e5f3a6e2ca6c894978395ae
Axel Van Gestel <r0784084@student.thomasmore.be>
2021-10-29 11:01:29 +0200

    Added: Initial playerDataController

--- src/main/java/com/example/back_playerdata_microservice/controller/PlayerDataController.java
+++ src/main/java/com/example/back_playerdata_microservice/controller/PlayerDataController.java
@@ -1,12 +1,78 @@
 package com.example.back_playerdata_microservice.controller;
 
+import com.example.back_playerdata_microservice.model.PlayerData;
 import com.example.back_playerdata_microservice.repository.PlayerDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class PlayerDataController {
 
     @Autowired
     private PlayerDataRepository playerDataRepository;
+
+    
+    @GetMapping("/playerDatas")
+    public List<PlayerData> getAll() {
+        return playerDataRepository.findPlayerDatas();
+    }
+
+    @RequestMapping(value="/playerData/{id}",
+            method= RequestMethod.GET,
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public PlayerData getPlayerDataById(@PathVariable String id){
+        return playerDataRepository.findPlayerDataById(id);
+    }
+
+    @RequestMapping(value="/playerData/{playerDataCode}",
+            method= RequestMethod.GET,
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public PlayerData getPlayerDataByPlayerDataCode(@PathVariable String playerDataCode){
+        return playerDataRepository.findPlayerDataByPlayerDataCode(playerDataCode);
+    }
+
+    @GetMapping("/playerDatas/type/{typeName}")
+    public List<PlayerData> getPlayerDatasByTypeName(@PathVariable String typeName){
+        return playerDataRepository.findPlayerDatasByTypeNameContaining(typeName);
+    }
+
+    @PostMapping("/playerData")
+    public PlayerData addPlayerData(@RequestBody PlayerData playerData) {
+        playerDataRepository.save(playerData);
+        return playerData;
+    }
+
+    @PutMapping("/playerData")
+    public PlayerData modifyPlayerData(@RequestBody PlayerData updatedPlayerData) {
+        PlayerData retrievedPlayerData = playerDataRepository.findPlayerDataByPlayerDataCode(updatedPlayerData.getPlayerDataCode());
+
+        retrievedPlayerData.setPlayerDataCode(updatedPlayerData.getPlayerDataCode());
+        retrievedPlayerData.setTypeName(updatedPlayerData.getTypeName());
+        retrievedPlayerData.setName(updatedPlayerData.getName());
+        retrievedPlayerData.setHealth(updatedPlayerData.getHealth());
+        retrievedPlayerData.setHappiness(updatedPlayerData.getHappiness());
+        retrievedPlayerData.setLastFed(updatedPlayerData.getLastFed());
+        retrievedPlayerData.setLastPetted(updatedPlayerData.getLastPetted());
+        retrievedPlayerData.setAge(updatedPlayerData.getAge());
+
+        playerDataRepository.save(retrievedPlayerData);
+
+        return retrievedPlayerData;
+    }
+
+    @DeleteMapping("/types/name/{playerDataCode}")
+    public ResponseEntity deletePlayerDataCode(@PathVariable String playerDataCode) {
+        PlayerData playerData = playerDataRepository.findPlayerDataByPlayerDataCode(playerDataCode);
+        if (playerData!=null) {
+            playerDataRepository.delete(playerData);
+            return ResponseEntity.ok().build();
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
 }
